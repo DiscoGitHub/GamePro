8bd8cdbc06de2c24fc2a5a5d376679a5d90e5794
dwarftee <fuladibaba@gmail.com>
2024-08-01 17:17:15 +0100

    commit :0

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
-class Main
-{
-    public static void main(String []args)
-    {
-        System.out.println("wassup gang");
+import javax.swing.*;
+import java.awt.Font;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("pookie bear");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(800, 600);
+
+        JLabel label = new JLabel("jay is my pookie", SwingConstants.CENTER);
+
+        label.setFont(new Font("Arial", Font.PLAIN, 24));
+
+        frame.add(label);
+
+        frame.setVisible(true);
     }
-};
+}
