63bcf319a9e2f01fad534ad23a0dba55397a59d5
WebDev <112544414+DiscoGitHub@users.noreply.github.com>
2024-08-02 08:52:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ import java.awt.Font;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Rizzy");
+        JFrame frame = new JFrame("GBrowserBeta");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setSize(1000, 700);
 
-        JLabel label = new JLabel("jay is my pookie", SwingConstants.CENTER);
+        JLabel label = new JLabel("Loading...", SwingConstants.CENTER);
 
         label.setFont(new Font("Arial", Font.PLAIN, 50));
 
